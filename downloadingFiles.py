e90e23c4cb4e59065f333d02c70fa3c26e57bb68
ArseniyBeglov <HUDAIDOTT@yandex.ru>
2023-10-14 01:51:05 +0300

    beta v1.6 version(solving problems with ecxel)

--- downloadingFiles.py
+++ downloadingFiles.py
@@ -10,7 +10,11 @@ links_for_files = ['https://inefb.ru/images/Raspisanie/Ochka/1kyrs.xls',
                    'https://inefb.ru/images/Raspisanie/Mag_Ochka/mag1.xls',
                    'https://inefb.ru/images/Raspisanie/Mag_Ochka/mag2.xls',
                    'https://inefb.ru/images/Raspisanie/Ochka_Zaochka/4kyrsOZ.xls',
-                   'https://inefb.ru/images/Raspisanie/Ochka_Zaochka/2kyrsOZ.xls']
+                   'https://inefb.ru/images/Raspisanie/Ochka_Zaochka/2kyrsOZ.xls',
+                   'https://inefb.ru/images/Raspisanie/Mag_Ochka/mag1oz.xls',
+                   'https://inefb.ru/images/Raspisanie/Ochka_Zaochka/1kyrsOZ.xls']
+
+
 
 
 def downloading_files(list_links):
@@ -25,7 +29,8 @@ def downloading_files(list_links):
 
 
 def updating_schedule():
-    list = ['1kyrs.xlsx', '2kyrs.xlsx', '3kyrs.xlsx', '4kyrs.xlsx', 'mag1.xlsx', 'mag2.xlsx', '4kyrsOZ.xlsx', '2kyrsOZ.xlsx']
+    list = ['1kyrs.xlsx', '2kyrs.xlsx', '3kyrs.xlsx', '4kyrs.xlsx', 'mag1.xlsx', 'mag2.xlsx', '4kyrsOZ.xlsx',
+            '2kyrsOZ.xlsx','1kyrsOZ.xlsx','mag1oz.xlsx' ]
 
     for val in list:
         if os.path.exists(val):
--- functionsSchedule.py
+++ functionsSchedule.py
@@ -5,6 +5,15 @@ from openpyxl import load_workbook
 import datetime
 import re
 
+def date_check(date_str_checking):
+    if '..' in date_str_checking:
+        date_str_checking=date_str_checking.replace('..','.')
+
+    date_line = re.search('\d{2}-\d{2}-\d{4}|\d{2}.\d{2}.\d{4}|\d{2}.\d{2}.\d{2}|\d{2}..\d{2}.\d{4}',
+                              date_str_checking)
+    date = datetime.datetime.strptime(date_line.group(), '%d.%m.%Y').date()
+    return date
+
 
 def processing(filename):
     dict_teach_and_group = {}
@@ -20,39 +29,39 @@ def processing(filename):
             if row[0].value is not None and row[0].value.split()[0] in weeks:
                 week = row[0].value
                 (week, day) = week.split()
-            elif not isinstance(row[0], openpyxl.cell.cell.MergedCell):
+            elif not  isinstance(row[0], openpyxl.cell.cell.MergedCell):
                 if row[1].value == "Время":
                     # if not any(val.value is None for val in row):
                     group_row = row
                 continue
 
             time = row[1].value
-
-            dict_teach_and_group[week, day, time] = {}
-            week_day_time = (week, day, time)
-            for i in range(2, len(row)):
-                group = group_row[i].value
-                group = str(group)
-                if 'Общая физическая подготовка пз' in str(row[i].value):
-                    break
-                if isinstance(row[i], openpyxl.cell.cell.MergedCell) and val is not None:
-                    dict_teach_and_group[week_day_time][val] += [group.strip()]
-                elif row[i].value is not None:
-                    val = row[i].value
-                    val = ' '.join(val.split())
-                    if val not in dict_teach_and_group[week, day, time]:
-                        dict_teach_and_group[week_day_time][val] = [group.strip()]
+            if datetime.date.today() <= date_check(day):
+                week_day_time = (week, day, time)
+                dict_teach_and_group[week_day_time] = {}
+                for i in range(2, len(row)):
+                    group = group_row[i].value
+                    group = str(group)
+                    if 'Общая физическая подготовка пз'  in str(row[i].value):
+                        break
+                    if isinstance(row[i], openpyxl.cell.cell.MergedCell) and val is not None:
+                        if val not in dict_teach_and_group[week_day_time]:
+                            dict_teach_and_group[week_day_time][val] = [group.strip()]
+                        else:
+                            dict_teach_and_group[week_day_time][val] += [group.strip()]
+                    elif row[i].value is not None:
+                        val = row[i].value
+                        val = ' '.join(val.split())
+                        if val not in dict_teach_and_group[week, day, time]:
+                            dict_teach_and_group[week_day_time][val] = [group.strip()]
+                        else:
+                            dict_teach_and_group[week_day_time][val] += [group.strip()]
                     else:
-                        dict_teach_and_group[week_day_time][val] += [group.strip()]
-                else:
-                    val = None
+                        val = None
     return dict_teach_and_group
 
 
-def date_check(date_str_checking):
-    date_line = re.search('\d{2}-\d{2}-\d{4}|\d{2}.\d{2}.\d{4}|\d{2}.\d{2}.\d{2}|\d{2}..\d{2}.\d{4}', date_str_checking)
-    date = datetime.datetime.strptime(date_line.group(), '%d.%m.%Y').date()
-    return date
+
 
 
 def toStr(timedate, subgroups,form, typeT, flag=False):
@@ -72,12 +81,11 @@ def printing_schedule(flag):
     if flag == 'очное':
         flist = ['1kyrs.xlsx', '2kyrs.xlsx', '3kyrs.xlsx', '4kyrs.xlsx', 'mag1.xlsx', 'mag2.xlsx']
     elif flag == 'очно-заочное':
-        flist = ['2kyrsOZ.xlsx','4kyrsOZ.xlsx']
+        flist = ['1kyrsOZ.xlsx','2kyrsOZ.xlsx','4kyrsOZ.xlsx','mag1oz.xlsx']
     else:
         flist = []
     lists = [[y+(checkForm(x),) for y in processing(x).items()] for x in flist]
     vaiues = list(chain(*lists))
-
     vaiues = [(x[0], {z: x[1][z] for z in x[1] if 'Янгирова' in z}, x[2]) for x in vaiues if
               any([True for y in x[1] if 'Янгирова' in y])]
 
@@ -89,6 +97,4 @@ def printing_schedule(flag):
     for vaiue in vaiues:
         newVaiues += toStr(*vaiue, flag, day != vaiue[0][1])
         day = vaiue[0][1]
-    print(newVaiues)
     return newVaiues
-
